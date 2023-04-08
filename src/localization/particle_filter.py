19dc5048f1daa69c5e9d0dbe26807da5e50ecf7f
nsthakur <nsthakur@mit.edu>
2023-04-08 18:01:48 -0400

    fixed merges + path working

--- src/localization/particle_filter.py
+++ src/localization/particle_filter.py
@@ -5,16 +5,19 @@ import tf.transformations as tf
 from sensor_model import SensorModel
 from motion_model import MotionModel
 import numpy as np
+import tf2_ros
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
-from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseWithCovarianceStamped
-from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Point
 from geometry_msgs.msg import PoseArray
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import Quaternion
 import threading
+from std_msgs.msg import Float32MultiArray
+import math
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
 
 
 class ParticleFilter:
@@ -26,6 +29,9 @@ class ParticleFilter:
         self.flag = 0 
         self.speed = None
 
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
+
         # Initialize publishers/subscribers
         #
         #  *Important Note #1:* It is critical for your particle
@@ -35,6 +41,7 @@ class ParticleFilter:
         #     a twist component, you will only be provided with the
         #     twist component, so you should rely only on that
         #     information, and *not* use the pose component.
+        
         scan_topic = rospy.get_param("~scan_topic", "/scan")
         odom_topic = rospy.get_param("~odom_topic", "/odom")
 
@@ -59,6 +66,8 @@ class ParticleFilter:
                                           self.pose_initialization,
                                           queue_size=1)
 
+        # self.error_pub = rospy.Publisher("/error", Float32MultiArray, queue_size=1)
+
         #  *Important Note #3:* You must publish your pose estimate to
         #     the following topic. In particular, you must use the
         #     pose field of the Odometry message. You do not need to
@@ -73,19 +82,17 @@ class ParticleFilter:
         self.path_pub = rospy.Publisher('/path', Path, queue_size=1)
         
         self.path = Path()
-        
+
         self.num_particles = rospy.get_param("~num_particles", 200)
         self.particles = np.zeros((self.num_particles, 3))
         self.lock = threading.Lock() # for particle array self.particles
 
         self.probs = np.ones((self.num_particles,))
+        self.prev_x = None
+        self.prev_y = None
         
         
-        # Initialize the models
-        self.motion_model = MotionModel()
-        self.sensor_model = SensorModel()
 
-       
 
     # Implement the MCL algorithm
     # using the sensor model and the motion model
@@ -96,6 +103,9 @@ class ParticleFilter:
     #
     # Publish a transformation frame between the map
     # and the particle_filter_frame.
+
+    def euler_from_quaternion(self, quaternion):
+        return tf.euler_from_quaternion([quaternion.x, quaternion.y, quaternion.z, quaternion.w])
 
     def publish_particles(self):
         particle_array = PoseArray()
@@ -133,7 +143,12 @@ class ParticleFilter:
         new_x = np.average(particles[:,0], weights=probs)
         new_y = np.average(particles[:,1], weights=probs)
 
-        # rospy.loginfo(particles[0])
+        if self.prev_x is not None:
+            new_x = (1-tau)*new_x + tau*self.prev_x
+            new_y = (1-tau)*new_y + tau*self.prev_y
+
+        self.prev_x = new_x
+        self.prev_y = new_y
 
         angles = self.particles[:,2]
         angle_vecs = np.array([np.cos(angles), np.sin(angles)])
@@ -159,9 +174,6 @@ class ParticleFilter:
         odom_msg.pose.pose.orientation = orientation
         odom_msg.header.stamp = rospy.Time.now()
 
-        # rospy.loginfo(point)
-        # rospy.loginfo(orientation)
-
         self.odom_pub.publish(odom_msg)
 
         # path message 
@@ -173,6 +185,19 @@ class ParticleFilter:
 
         self.path.poses.append(pose)
         self.path_pub.publish(self.path)
+
+        ########## GRAPHING ERROR ######################
+
+        # error = Float32MultiArray()
+
+        # transform = self.tfBuffer.lookup_transform("map", "base_link", rospy.Time(), rospy.Duration(1.0))
+        # real_x = transform.transform.translation.x
+        # real_y = transform.transform.translation.y
+        # real_theta = self.euler_from_quaternion(transform.transform.rotation)[2]
+
+        # error.data = ([new_x - real_x, new_y - real_y, abs(real_theta-new_angle)])
+
+        # self.error_pub.publish(error)
 
     def lidar_callback(self, lidar_data):
 
@@ -216,10 +241,13 @@ class ParticleFilter:
         # Get the "average" particle through a weighted average
         self.publish_average_point(self.particles, self.probs)
         self.publish_particles()
-
-        # rospy.loginfo(self.particles)
+        
     
     def pose_initialization(self, pose_data):
+
+        self.prev_x = None
+        self.prev_y = None
+        self.Path = Path()
 
         position = pose_data.pose.pose.position
         q = pose_data.pose.pose.orientation
@@ -247,6 +275,6 @@ class ParticleFilter:
 
 
 if __name__ == "__main__":
-    rospy.init_node("path_node")#("particle_filter")
+    rospy.init_node("particle_filter")
     pf = ParticleFilter()
     rospy.spin()
